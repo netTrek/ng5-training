abd1507df53811ddfd600c7452ca24b101fbcde7
Saban Uenlue - netTrek <us@netTrek.de>
2017-12-11 23:57:16 +0100

    locale and slides

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then ( () => {
+    // registerLocaleData( localeDe );
+    // or ng serve --locale=de
+    console.log ( 'sa' );
+  } )
   .catch(err => console.log(err));
