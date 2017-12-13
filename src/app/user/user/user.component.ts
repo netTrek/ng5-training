97e0c2c1cc78320c2f47cd0823e96eecd152541f
Saban Uenlue - netTrek <us@netTrek.de>
2017-12-13 15:18:51 +0100

    HostBindings

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter, HostListener, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import {
+  Component, EventEmitter, HostBinding, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component ( {
-  selector   : 'bm-user',
-  templateUrl: './user.component.html',
+  selector     : 'bm-user',
+  templateUrl  : './user.component.html',
   // template : `
   //   <hr>
   //   hello <br>
   //   <strong (click)="clickMe()">world</strong>
   //   <hr>
   // `,
-  styleUrls  : [ './user.component.scss' ],
+  styleUrls    : [ './user.component.scss' ],
   // styles: [ `
   //
   //
@@ -21,21 +24,44 @@ import { Component, EventEmitter, HostListener, Input, OnInit, Output, ViewEncap
   //           ` ]
   encapsulation: ViewEncapsulation.Emulated // Native
 } )
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnChanges {
 
-  @Input()
+  @Input ()
   name = 'unknown';
 
-  @Output()
-  selected: EventEmitter<string> = new EventEmitter();
+  @Input ()
+  selectedUsrName: string;
+
+  @Output ()
+  selected: EventEmitter<string> = new EventEmitter ();
+
+  @HostBinding ( 'class.selected' )
+  isSelected = false;
+
+  @HostBinding ( 'style.color' )
+  color: string | null = null;
 
   constructor () {
   }
 
-  @HostListener( 'click', [ '$event', 'name'] )
+  @HostListener ( 'click', [ '$event',
+                             'name'
+  ] )
   clickMe ( evt: MouseEvent, name: string ) {
     console.log ( evt, name );
-    this.selected.next( this.name );
+    this.selected.next ( this.name );
+  }
+
+  ngOnChanges ( changes: SimpleChanges ): void {
+    if ( !! changes.selectedUsrName ) {
+      this.isSelected = this.selectedUsrName === this.name;
+      this.color = this.isSelected ? 'blue' : null;
+      // if ( this.isSelected ) {
+      //   this.color = 'blue';
+      // } else {
+      //   this.color = null;
+      // }
+    }
   }
 
   ngOnInit () {
