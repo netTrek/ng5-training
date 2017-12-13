4b1c34cd07586ad3c3775f2bf0a85691d7b521b4
Saban Uenlue - netTrek <us@netTrek.de>
2017-12-13 11:45:06 +0100

    ContentChild

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ContentChildren, OnInit, QueryList } from '@angular/core';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'bm-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  @ContentChild (UserComponent)
+  user: UserComponent;
+
+  @ContentChildren (UserComponent)
+  users: QueryList<UserComponent>;
+
+  constructor( ) { }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit (): void {
+    console.log( this.user, this.users.toArray()  );
+  }
 }
