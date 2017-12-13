b19a1cf49e789bbe43ed94d18883ccd09c60a9e3
Saban Uenlue - netTrek <us@netTrek.de>
2017-12-13 11:15:37 +0100

    ng-content

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserGroupComponent } from './user-group/user-group.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserComponent, UserListComponent],
-  exports: [UserComponent, UserListComponent]
+  declarations: [UserComponent, UserListComponent, UserGroupComponent],
+  exports: [UserComponent, UserListComponent, UserGroupComponent]
 })
 export class UserModule { }
